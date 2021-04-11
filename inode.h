d3c398a9b5cd590ebe4473f5559c0455153a5746
SubhanKhan-458 <k191285@nu.edu.pk>
2021-04-11 17:24:34 +0500

    Adding inode

--- inode.h
+++ inode.h
@@ -1,5 +1,5 @@
-#ifndef _DISK_H_
-#define _DISK_H_
+#ifndef _inode.h_
+#define _inode.h_
 
 
 #include <stdio.h>
@@ -20,25 +20,26 @@
 struct inode // 4Byte
 {
     int filesize;
+    int pointer_blocks[256];    //Per file 1MB=1048576/4096
 };
 
-struct file_to_inode_mapping // 36Byte
+struct mapping // 36Byte
 {
     char file_name[30]; 
     int inode_num;      
 };
 
-struct super_block 
+struct superblock 
 {
-    bool inode_freelist[NO_OF_INODES];    
-    bool datablock_freelist[DISK_BLOCKS]; 
+    bool inode_list[NO_OF_INODES];    
+    bool datablock_list[DISK_BLOCKS]; 
     
     //6*2 + 131072 + 78644
     //Blocks = 209740/4096 = 52 blocks 
-    int no_of_blocks_used_by_superblock = ceil(((float)sizeof(super_block)) / BLOCK_SIZE);
+    int no_of_blocks_used_by_superblock = ceil(((float)sizeof(superblock)) / BLOCK_SIZE);
 
     //mapping b/w filename and inode is 36*78644/4096 = 692  
-    int no_of_blocks_used_by_file_inode_mapping = ceil(((float)sizeof(struct file_to_inode_mapping) * NO_OF_INODES) / BLOCK_SIZE);
+    int no_of_blocks_used_by_file_inode_mapping = ceil(((float)sizeof(struct mapping) * NO_OF_INODES) / BLOCK_SIZE);
 
     //Inode starts at 744 
     int starting_index_of_inodes = no_of_blocks_used_by_superblock + no_of_blocks_used_by_file_inode_mapping;
